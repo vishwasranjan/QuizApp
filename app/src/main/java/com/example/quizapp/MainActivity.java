92b7b5a6df7cbf29df6e321d2ba09adf56511b58
vishwasranjan <vishwasranjan16@gmail.com>
2020-09-13 23:10:18 +0530

    Second commit

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
